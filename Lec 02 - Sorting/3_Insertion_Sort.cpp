55e51363bd6262c3c798d2f804e2aa9a7547288e
Sagar Udasi <43277742+I-am-Sagar@users.noreply.github.com>
2019-07-05 01:03:37 +0530

    Sorting added

--- Lec 02 - Sorting/3_Insertion_Sort.cpp
+++ Lec 02 - Sorting/3_Insertion_Sort.cpp
@@ -44,14 +44,14 @@ void insertionSort1 (int arr[], int n) {
 	for (i = 1; i < n; i++) { 
       	key = arr[i]; 
       	j = i-1; 
-		// Move elements of arr[0..i-1], that are
-		// greater than key, to one position ahead
-		// of their current position
+	// Move elements of arr[0..i-1], that are
+	// greater than key, to one position ahead
+	// of their current position
        	while (j >= 0 && arr[j] > key) {			 			 
         	arr[j+1] = arr[j];			 
            	j = j-1;
        	} 
-		// Cant replace key by arr[i] or arr[j+1] = arr[i]
+	// Cant replace key by arr[i] or arr[j+1] = arr[i]
        	arr[j+1] = key;
    	} 
 }
