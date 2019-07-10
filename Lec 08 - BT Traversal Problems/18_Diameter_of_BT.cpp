98a37e2538401c778dfef25183f146c1cb8f8ab5
I-am-Sagar <sagar.udasi.official@gmail.com>
2019-07-10 22:18:54 +0530

    Traversal Problems added

---
+++ Lec 08 - BT Traversal Problems/18_Diameter_of_BT.cpp
@@ -0,0 +1,83 @@
+/*
+    The diameter of a tree is the number of nodes on the longest 
+    path between two end nodes.
+
+    Basic observation:
+    The diameter of a tree T is the largest of the following quantities:
+    a) the diameter of T’s left subtree
+    b) the diameter of T’s right subtree
+    c) the longest path between leaves that goes through the root of T 
+       (this can be computed from the heights of the subtrees of T)
+    
+    The code of above implementation is:
+    
+        int diameter (Node* root) {
+            if (tree == NULL)   return 0;
+
+            int l_height = height(tree->left);
+            int r_height = height(tree->right);
+
+            int l_diameter = diameter(tree->left);
+            int r_diameter = diameter(tree->right);
+
+            return max(l_height+r_height+1, max(l_diameter, r_diameter));
+        }
+    
+    Time Complexity: O(n^2)
+
+    Here is another way to think about diameter of tree:
+    Diameter of a tree can be calculated by only using the height function, 
+    because the diameter of a tree is nothing but maximum value of 
+    (left_height + right_height + 1) for each node. So we need to calculate 
+    this value (left_height + right_height + 1) for each node and 
+    update the result. 
+
+    Time Complexity: O(n)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left, *right;
+        Node (int data) {
+            this->data = data;
+            this->left = this->right = NULL;
+        }
+};
+
+// Function to compute height
+int height (Node* root, int &ans) {
+    if (root == NULL)   return 0;
+
+    int left_height = height(root->left, ans);
+    int right_height = height(root->right, ans);
+    
+    ans = max(ans, 1+left_height+right_height);
+
+    return (1 + max(left_height, right_height));
+}
+
+// Computes the diameter of binary tree
+int diameter (Node* root) {
+    if (root == NULL)   return 0;
+    int ans = INT_MIN;
+    int height_of_tree = height(root, ans);
+    return ans;
+}
+
+/* ----------------- MAIN DRIVER CODE ------------- */
+int main() 
+{ 
+    Node* root = new Node(1); 
+    root->left = new Node(2); 
+    root->right = new Node(3); 
+    root->left->left = new Node(4); 
+    root->left->right = new Node(5); 
+  
+    printf("Diameter is %d\n", diameter(root)); 
+  
+    return 0; 
+} 
---
+++ Lec 08 - BT Traversal Problems/6_Print_leftmost_rightmost_nodes.cpp
@@ -0,0 +1,83 @@
+/*  PROBLEM:
+    Given a BT, print the corner nodes at each level. 
+    The node at the leftmost and the node at the rightmost.
+
+    For example, for given tree:
+                1
+               / \
+              2   3
+            /  \ /  \
+           7   6 5   4
+    
+    Output: 1 2 3 7 4
+
+    SOLUTION:
+
+    Approach 1:
+    A Simple Solution is to do two traversals using the approaches 
+    discussed in the earlier question for printing left view and 
+    right view.
+
+    Approach 2:
+    The idea is to use Level Order Traversal. Every time we store 
+    the size of the queue in a variable n, which is the number of 
+    nodes at that level. For every level we check three conditions, 
+    whether there is one node or more than one node, in case there 
+    is only one node we print it once and in case we have more than 
+    1 nodes, we print the first (i.e node at index 0) and the node 
+    at last index (i.e node at index n-1).
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* left, *right;
+        Node (int data) {
+            this->data = data;
+            this->left = this->right = NULL;
+        }
+};
+
+// Main Logic of Code
+void printCorner (Node* root) {
+    if (root == NULL)   return;
+    
+    queue<Node*> q;
+    q.push(root);
+    vector<int> result; //This will store answer
+    while (!q.empty()) {
+        int n = q.size();
+        for (int i = 0; i < n; i++) {
+            Node* temp = q.front();
+            q.pop();
+            // leftmost corner value
+            if (i == 0)
+                result.push_back(temp->data);
+            // rightmost corner value
+            else if (i == n-1)
+                result.push_back(temp->data);
+            
+            if (temp->left) q.push(temp->left);
+            if (temp->right) q.push(temp->right);
+        }
+    }
+    for (auto i : result)
+        cout << i << " ";
+}
+
+/* ---------------- MAIN DRIVER CODE ----------------- */
+int main () 
+{ 
+    Node *root =  new Node(15); 
+    root->left = new Node(10); 
+    root->right = new Node(20); 
+    root->left->left = new Node(8); 
+    root->left->right = new Node(12); 
+    root->right->left = new Node(16); 
+    root->right->right = new Node(25); 
+    printCorner(root); 
+    return 0;  
+} 
